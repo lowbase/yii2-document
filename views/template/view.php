268e5fdede2c9f428622741a8a20c627919074e2
lowBase <lowbase@yandex.ru>
2016-04-06 02:04:37 +0300

    Update view.php

--- views/template/view.php
+++ views/template/view.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * @package   yii2-document
+ * @author    Yuri Shekhovtsov <shekhovtsovy@yandex.ru>
+ * @copyright Copyright &copy; Yuri Shekhovtsov, lowbase.ru, 2015 - 2016
+ * @version   1.0.0
+ */
+ 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
 use lowbase\document\DocumentAsset;
