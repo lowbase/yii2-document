16b35f18c87c5847148e1f1367ba3b30bae05599
lowBase <lowbase@yandex.ru>
2016-04-29 01:35:32 +0300

    Update ValueText.php

--- models/ValueText.php
+++ models/ValueText.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ * @package   yii2-document
+ * @author    Yuri Shekhovtsov <shekhovtsovy@yandex.ru>
+ * @copyright Copyright &copy; Yuri Shekhovtsov, lowbase.ru, 2015 - 2016
+ * @version   1.0.0
+ */
+ 
 namespace lowbase\document\models;
 
 use Yii;
