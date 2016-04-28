25e2cb795f2e8315eff0b38f6fc21e9c9716a79f
lowBase <lowbase@yandex.ru>
2016-04-29 01:12:21 +0300

    Update create.php

--- views/field/create.php
+++ views/field/create.php
@@ -13,7 +13,7 @@ use yii\helpers\Html;
 /* @var $model app\modules\document\models\Field */
 
 $this->title = Yii::t('document', 'Новое поле');
-$this->params['breadcrumbs'][] = ['label' => Yii::t('document', 'Шаблоны'), 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => Yii::t('document', 'Шаблоны'), 'url' => ['template/index']];
 $this->params['breadcrumbs'][] = ['label' => $template->name, 'url' => ['template/view', 'id' => $template->id]];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
