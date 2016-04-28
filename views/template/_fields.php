0081a2a13875568dd759af8c86e8c2e5e016f096
lowBase <lowbase@yandex.ru>
2016-04-28 23:23:15 +0300

    Create _fields.php

---
+++ views/template/_fields.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @package   yii2-document
+ * @author    Yuri Shekhovtsov <shekhovtsovy@yandex.ru>
+ * @copyright Copyright &copy; Yuri Shekhovtsov, lowbase.ru, 2015 - 2016
+ * @version   1.0.0
+ */
+
+use kartik\grid\GridView;
+use lowbase\document\models\Field;
+use yii\helpers\Html;
+
+$gridColumns = [
+    [
+        'class' => 'kartik\grid\SerialColumn',
+        'contentOptions' => ['class'=>'kartik-sheet-style'],
+        'width' => '30px',
+        'header' => '',
+        'headerOptions' => ['class' => 'kartik-sheet-style']
+    ],
+    [
+        'attribute' => 'id',
+        'width' => '70px',
+    ],
+    'name',
+    [
+        'attribute' => 'type',
+        'value' => function ($model) {
+            return Field::getTypes()[$model->id];
+        },
+        'filter' => Field::getTypes(),
+    ],
+    'min',
+    'max',
+    [
+        'template' => '{update} {delete}',
+        'class'=>'kartik\grid\ActionColumn',
+    ],
+    [
+        'class'=>'kartik\grid\CheckboxColumn',
+        'headerOptions'=>['class'=>'kartik-sheet-style'],
+    ],
+];
+
+echo GridView::widget([
+    'layout'=>"{items}\n{summary}\n{pager}",
+    'dataProvider'=> $dataProvider,
+    'filterModel' => $searchModel,
+    'columns' => $gridColumns,
+    'containerOptions' => ['style'=>'overflow: auto'],
+    'headerRowOptions' => ['class'=>'kartik-sheet-style'],
+    'filterRowOptions' => ['class'=>'kartik-sheet-style'],
+    'pjax' => false,
+    'panel' => [
+        'heading' => '<i class="glyphicon glyphicon-th-list"></i> '.Yii::t('document', 'Дополнительные поля'),
+        'type' => GridView::TYPE_PRIMARY,
+        'before' => Html::a('<span class="glyphicon glyphicon-plus"></span> '.Yii::t('document', 'Добавить'), [
+            'field/create', 'template_id' => $model->id], ['class' => 'btn btn-success']),
+        'after' => "<div class='text-right'><b>".Yii::t('document', 'Выбранные').":</b> ".
+            Html::button('<span class="glyphicon glyphicon-trash"></span> '.Yii::t('document', 'Удалить'), [
+                'class' => 'btn btn-danger delete-all']).
+            "</div>"
+    ],
+    'export' => [
+        'fontAwesome' => true
+    ],
+    'bordered' => true,
+    'striped' => true,
+    'condensed' => true,
+    'persistResize' => false,
+    'hover' => true,
+    'responsive' => true,
+]);
+?>
