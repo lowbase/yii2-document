f508d0e801c2a01cdc9413d911b7390ecd9a701c
lowBase <lowbase@yandex.ru>
2016-04-29 00:33:14 +0300

    Update _form.php

--- views/template/_form.php
+++ views/template/_form.php
@@ -6,18 +6,16 @@
  * @version   1.0.0
  */
 
-use kartik\widgets\Select2;
-use lowbase\document\models\Field;
 use lowbase\document\models\Template;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
-/* @var $model app\modules\document\models\Field */
+/* @var $model app\modules\document\models\Template */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="field-form">
+<div class="template-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
@@ -30,53 +28,32 @@ use yii\widgets\ActiveForm;
                     echo Html::a('<i class="glyphicon glyphicon-trash"></i> '.Yii::t('document', 'Удалить'), ['delete', 'id' => $model->id], [
                         'class' => 'btn btn-danger',
                         'data' => [
-                            'confirm' => Yii::t('document', 'Вы уверены, что хотите удалить поле?'),
+                            'confirm' => Yii::t('document', 'Вы уверены, что хотите удалить шаблон?'),
                             'method' => 'post',
                         ],
                     ]);
-                echo " " . Html::a('<i class="glyphicon glyphicon-menu-left"></i> '.Yii::t('document', 'Отмена'), ['template/update', 'id' => $model->template_id], [
-                    'class' => 'btn btn-default']);
-                } else {
-                    echo Html::a('<i class="glyphicon glyphicon-menu-left"></i> '.Yii::t('document', 'Отмена'), ['template/update', 'id' => $template->id], [
-                        'class' => 'btn btn-default']);
-                } ?>
+                }
+                ?>
+                <?= Html::a('<i class="glyphicon glyphicon-menu-left"></i> '.Yii::t('document', 'Отмена'), ['index'], [
+                    'class' => 'btn btn-default',
+                ]) ?>
             </p>
         </div>
     </div>
+
 
     <div class="row">
         <div class="col-lg-6">
             <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
         </div>
         <div class="col-lg-6">
-            <?= $form->field($model, 'template_id')->widget(Select2::classname(), [
-                'data' => Template::getAll(),
-                'options' => [
-                    'placeholder' => '',
-                    'id' => 'template_id'
-                ],
-                'pluginOptions' => [
-                    'allowClear' => true
-                ],
-            ]); ?>
+            <?= $form->field($model, 'path')->textInput(['maxlength' => true]) ?>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-lg-6">
-            <?= $form->field($model, 'type')->dropDownList(Field::getTypes()) ?>
-        </div>
-        <div class="col-lg-6">
-            <?= $form->field($model, 'param')->textInput(['maxlength' => true]) ?>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-lg-6">
-            <?= $form->field($model, 'min')->textInput() ?>
-        </div>
-        <div class="col-lg-6">
-            <?= $form->field($model, 'max')->textInput() ?>
+        <div class="col-lg-12">
+            <?= $form->field($model, 'description')->textarea(['rows' => 6]) ?>
         </div>
     </div>
 
