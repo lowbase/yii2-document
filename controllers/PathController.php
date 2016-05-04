bdf14190801845717644f244a7ca7318a0a6eed5
lowBase <lowbase@yandex.ru>
2016-05-05 01:48:13 +0300

    Create PathController.php

---
+++ controllers/PathController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package   yii2-document
+ * @author    Yuri Shekhovtsov <shekhovtsovy@yandex.ru>
+ * @copyright Copyright &copy; Yuri Shekhovtsov, lowbase.ru, 2015 - 2016
+ * @version   1.0.0
+ */
+
+namespace app\controllers;
+
+//use yii\filters\AccessControl;
+
+class PathController extends \mihaildev\elfinder\PathController {
+
+    public $root = [
+        'baseUrl'=>'',
+        'basePath'=>'@app/web',
+        'path' => 'attach/document',
+        'name' => 'Файлы',
+    ];
+
+    public function behaviors()
+    {
+        return [
+            // Ограничение доступа к операциям, связанным с файловым менеджером
+            // Активировать при подключении пользователей и разделений прав
+//            'access' => [
+//                'class' => AccessControl::className(),
+//                'only' => ['manager', 'connect'],
+//                'rules' => [
+//                    [
+//                    ],
+//                ],
+//            ],
+        ];
+    }
+
+}
